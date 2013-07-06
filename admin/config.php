e2dbfba60ef279392995ea42842c6257f990d075
Tuan <dinhtuan0108@gmail.com>
2013-07-06 23:54:14 +0700

    int project opencart 1.5.5.1

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/offshore_open/admin/');
+define('HTTP_CATALOG', 'http://localhost/offshore_open/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/offshore_open/admin/');
+define('HTTPS_CATALOG', 'http://localhost/offshore_open/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/admin/');
+define('DIR_SYSTEM', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/system/');
+define('DIR_DATABASE', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/system/database/');
+define('DIR_LANGUAGE', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/admin/language/');
+define('DIR_TEMPLATE', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/admin/view/template/');
+define('DIR_CONFIG', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/system/config/');
+define('DIR_IMAGE', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/image/');
+define('DIR_CACHE', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/system/cache/');
+define('DIR_DOWNLOAD', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/download/');
+define('DIR_LOGS', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/system/logs/');
+define('DIR_CATALOG', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'offshore_open');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/offshore_open/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/offshore_open/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/catalog/');
+define('DIR_SYSTEM', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/system/');
+define('DIR_DATABASE', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/system/database/');
+define('DIR_LANGUAGE', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/catalog/language/');
+define('DIR_TEMPLATE', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/system/config/');
+define('DIR_IMAGE', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/image/');
+define('DIR_CACHE', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/system/cache/');
+define('DIR_DOWNLOAD', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/download/');
+define('DIR_LOGS', 'D:\WORKING\CODE\WEB\xampp\htdocs\offshore_open/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'offshore_open');
+define('DB_PREFIX', 'oc_');
+?>
